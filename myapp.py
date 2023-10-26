3cd820a064a823414ff476ee6968ad5c1f48751c
kalisetti <sivasankar.k2003@gmail.com>
2023-10-26 18:47:50 +0600

    Simple form submission

--- myapp.py
+++ myapp.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello There!</h1>"
 
+@app.route('/information')
+def info():
+    return "<h1>Just some information</h1>"
+
+@app.route('/user/<name>')
+def user(name):
+    return "<h1>Hi there {}!</h1>".format(name)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
